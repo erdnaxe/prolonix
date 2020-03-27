a2b0a70dde19c29bec7039c6570ee1c73ec2d1b4
Alexandre Iooss <erdnaxe@crans.org>
2020-03-27 10:49:46 +0100

    Change iso image name and add live user

--- config/iso-image.nix
+++ config/iso-image.nix
@@ -10,8 +10,23 @@
   ];
 
   # Make bootable EFI/MBR ISO with memtest86+
+  isoImage.isoBaseName = "prolonix";
   isoImage.volumeID = "PROLOGIN";
+  isoImage.appendToMenuLabel = " Prologin";
   isoImage.makeEfiBootable = true;
   isoImage.makeUsbBootable = true;
   boot.loader.grub.memtest86.enable = true;
+
+  # Create live user
+  users.users.live = {
+    isNormalUser = true;
+    extraGroups = [ "video" ];
+    home = "/home/live";
+    description = "Live User";
+    # Allow the graphical user to login without password
+    initialHashedPassword = "";
+  };
+
+  # Autologin on virtual consoles
+  services.mingetty.autologinUser = "live";
 }
