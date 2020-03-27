738f0864a6942bc1d2617c12dada635856ac80df
Alexandre Iooss <erdnaxe@crans.org>
2020-03-27 11:47:54 +0100

    Add SSH for remote debug

--- config/includes/common.nix
+++ config/includes/common.nix
@@ -46,4 +46,10 @@
 
   # Fix Plasma5 and Seahorse collision
   programs.ssh.askPassword = "${pkgs.gnome3.seahorse}/libexec/seahorse/ssh-askpass";
+
+  # SSH for remote debug
+  services.openssh.enable = true;
+  users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDSdhwnHXW4jJHhoz1lQfbA094VmH6bFZ0sia6R+NChmhWRn/iZ5Lm5ePwjLTP1NTJZAix0nmMa2jwkT65y2jxVRCfhPcSabRRgh9lg1nUQjNnmE8ggF6gQ4kfzR2cmnofmWRwmRWLShRdk8TKhD16C5ejOFenV8AQj4KeZ3mjALBJqGg3IT23DvL6LBhN/c+qbc5sLMwJEDH+n7b+Wm2PkpxwDsMbBoRDCKGmqhY/nBpgA48FOTRuclqhLEy0Etp9kjy4aYIvgFVTXWD1WTzHvO1551S/rrd7syg5XuGgOCoRJxnkL+i3kepC8r0gnQo0A6vAPR4tWIZq3dKS7PQD3dhU6v2pgevsKtAtITGWzZG13w7HUdHRvssYdKSzO6UE+F4zKAfX9G0QBWRMkYcl/inz7jHOWlKLaUsdeNzVMMb5/MYie/lkDwpukHYFgoAQ6oLviZ2ZUh98SphuXIAdrZ2t6yxqAXRO6dUPa2qHG29OXfy5+qxo3/SlaCExRnxluRXHDMaxmtC9MowHf/fr4PJ7nscKLxM+o4PWhM9MhWpfuytbUwZuGqe/ifYW7lIRy7SH2EUbE1SOThHxsOKtitkRf11Q2olv92EWtDHq94uQPNAFADUwuIdQY9PDrV/SiMc8w4hKj689qXoG1YB06k2JSuD2myacUA4B/nNzC0Q== erdnaxe@xion"
+  ];
 }
